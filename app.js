744fa3ac72840117072e3330b07070647a1f3b2b
Nico San Pedro <nicolassanpedro@gmail.com>
2023-02-26 20:52:24 +0100

    add middleware jwt validator

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const { requireAuth } = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -30,23 +31,5 @@ mongoose
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
-
-// // cookies
-// app.get("/set-cookies", (req, res) => {
-//   res.cookie("newUser", false);
-//   res.cookie("isEmployee", true, {
-//     maxAge: 1000 * 60 * 60 * 24,
-//     httpOnly: true,
-//   });
-
-//   res.send("you got the cookies!");
-// });
-
-// app.get("/read-cookies", (req, res) => {
-//   const cookies = req.cookies;
-//   // console.log(cookies.newUser);
-
-//   res.json(cookies);
-// });
