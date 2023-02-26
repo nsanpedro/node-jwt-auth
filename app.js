fa7c7f48eb7003395bad56bc759c5b0370881274
Nico San Pedro <nicolassanpedro@gmail.com>
2023-02-26 20:20:13 +0100

    add login func

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const authRoutes = require("./routes/authRoutes");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 dotenv.config();
 
 // middleware
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 
 // view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // database connection
 const dbURI = process.env.MONGO_DB_URL;
@@ -29,8 +29,8 @@ mongoose
   .catch((err) => console.log(err));
 
 // routes
-app.get("/", (req, res) => res.render("home"));
-app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
 // // cookies
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,20 @@
-const User = require("../models/User");
-const jwt = require("jsonwebtoken");
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const handleErrors = (error) => {
   let errorData = {
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   };
 
   const alreadyRegistered = error.code === 11000;
 
   if (alreadyRegistered) {
-    errorData.email = "that email is already registered";
+    errorData.email = 'that email is already registered';
     return errorData;
   }
 
-  if (error.message.includes("user validation failed")) {
+  if (error.message.includes('user validation failed')) {
     Object.values(error?.errors).forEach(({ properties }) => {
       errorData[properties.path] = properties.message;
     });
@@ -26,12 +26,12 @@ const handleErrors = (error) => {
 //* jwt creation
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) =>
-  jwt.sign({ id }, "user secret", {
+  jwt.sign({ id }, 'user secret', {
     expiresIn: maxAge,
   });
 
 module.exports.signup_get = (req, res) => {
-  res.render("signup");
+  res.render('signup');
 };
 
 module.exports.signup_post = async (req, res) => {
@@ -40,7 +40,7 @@ module.exports.signup_post = async (req, res) => {
   try {
     const user = await User.create({ email, password });
     const jwtToken = createToken(user?._id);
-    res.cookie("jwt", jwtToken, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.cookie('jwt', jwtToken, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(201).json({ user: user?._id });
   } catch (error) {
     const errors = handleErrors(error);
@@ -49,11 +49,16 @@ module.exports.signup_post = async (req, res) => {
 };
 
 module.exports.login_get = (req, res) => {
-  res.render("login");
+  res.render('login');
 };
 
 module.exports.login_post = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req?.body;
 
-  res.send("login");
+  try {
+    const userData = await User.login(email, password);
+    res.status(200).json({ user: userData?._id });
+  } catch (err) {
+    res.status(400).json({});
+  }
 };
